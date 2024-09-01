868ba8b95a8c65400418fb242310791a7b54f745
ihascats <s.pavlovic.rs@gmail.com>
2024-09-01 06:59:34 +0200

    Remove false modifiers

--- index.js
+++ index.js
@@ -4,23 +4,20 @@ const fs = require('fs');
 const exibitions = JSON.parse(fs.readFileSync('exibitions.json', 'utf-8'));
 const groups = JSON.parse(fs.readFileSync('groups.json', 'utf-8'));
 
-let modifiers = {};
-function setModifier(obj, modifiersObject) {
+function setModifier(obj) {
+  // ---------------
   const exibitionsObject = obj;
   const size = Object.keys(exibitionsObject).length;
-  let mods = modifiersObject;
+  let mods = {};
+  // --------------
   for (let i = 0; i < size; i++) {
-    console.log('----------');
     let team = Object.keys(exibitionsObject)[i];
     let results = exibitionsObject[Object.keys(exibitionsObject)[i]];
     const exibitionSize =
       exibitionsObject[Object.keys(exibitionsObject)[i]].length;
-    console.log(team);
-    console.log(results);
-    console.log(results[0]['Result']);
     mods[team] = [];
+    // save positive modifiers
     for (let j = 0; j < exibitionSize; j++) {
-      console.log(j);
       let matchResult = results[j]['Result'];
       let [num1, num2] = matchResult.split('-').map(Number);
       let mod = num1 / num2;
@@ -31,10 +28,16 @@ function setModifier(obj, modifiersObject) {
         });
       }
     }
-    console.log('----------');
+    //-------------
+
+    // delete empty arrays
+    if (mods[team].length < 1) {
+      delete mods[team];
+    }
+    //-------------
   }
+  return mods;
 }
 
 console.log(groups['A'][0]);
-setModifier(exibitions, modifiers);
-console.log(modifiers);
+console.log(setModifier(exibitions));
